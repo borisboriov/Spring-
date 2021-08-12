e15d80a5f2a876444da42db623011be43dc435f4
Boris <soskinboris@gmail.com>
2021-08-12 22:08:48 +0300

    first commit

---
+++ src/main/java/spring/demo/spring/HelloWorldServlet.java
@@ -0,0 +1,16 @@
+package spring.demo.spring;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class HelloWorldServlet extends GenericServlet {
+
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+}

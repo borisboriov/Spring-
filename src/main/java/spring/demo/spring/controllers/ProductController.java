c9f83f3c88fab84e2d6e3d2e142dfac6c419800c
Boris <soskinboris@gmail.com>
2022-01-06 19:17:11 +0300

    HW#7.2 finish 1.1

---
+++ src/main/java/spring/demo/spring/controllers/ProductController.java
@@ -0,0 +1,59 @@
+package spring.demo.spring.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import spring.demo.spring.entities.Product;
+import spring.demo.spring.exceptions.ResourceNotFoundException;
+import spring.demo.spring.service.ProductService;
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+
+    @GetMapping("/products")
+    public List<Product> findAllProducts() {
+        return productService.findAllProducts();
+    }
+
+    @GetMapping("/products/{id}")
+    public Product findProductById(@PathVariable Long id) {
+        return productService.findById(id).orElseThrow(()-> new ResourceNotFoundException("Product not found, id:" + id));
+    }
+
+    @GetMapping("/products/delete/{id}")
+    public void deleteByID(@PathVariable Long id) {
+        productService.deleteByID(id);
+    }
+
+    //для сохранения продукта тут, пришлось создать отдельный конструктор без инта в сигнатуре
+    @GetMapping("/products/save/")
+    public void save(@RequestParam String title, @RequestParam int cost) {
+        productService.save(new Product(title, cost));
+    }
+
+
+    @GetMapping("/products/find_between")
+    public List<Product> findMax(@RequestParam(required = false) int min , @RequestParam(required = false) int max){
+        return productService.findAllByCostBetween(min, max);
+    }
+
+    @GetMapping("/products/find_greater")
+    public List<Product> findGreaterThen(@RequestParam(required = false) int min){
+        return productService.findGreaterThen(min);
+    }
+
+    @GetMapping("/products/find_less")
+    public List<Product> findLessThen(@RequestParam(required = false) int max){
+        return productService.findLessThen(max);
+    }
+
+}
